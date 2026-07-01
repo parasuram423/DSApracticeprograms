1f09ea65624796cfcc5ff0d9ae2091f377b76652
parasuram423 <parasurams423@gmail.com>
2026-07-01 11:27:29 +0530

    Update Password.java

--- src/com/practice/Password.java
+++ src/com/practice/Password.java
@@ -1,10 +1,24 @@
-package com.practice;
+ package com.practice;
+
+import java.util.Scanner;
 
 public class Password {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        Scanner sc = new Scanner(System.in);
 
+        String correctPassword = "Java123";
+
+        System.out.print("Enter Password: ");
+        String password = sc.nextLine();
+
+        if (password.equals(correctPassword)) {
+            System.out.println("Login Successful");
+        } else {
+            System.out.println("Invalid Password");
+        }
+
+        sc.close();
+    }
 }
