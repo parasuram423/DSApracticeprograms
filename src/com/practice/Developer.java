edddbf0d61e03e75f7a11e1194a77dfb775290ba
parasuram423 <parasurams423@gmail.com>
2026-07-01 11:25:03 +0530

    Add Developer class with display method

--- src/com/practice/Developer.java
+++ src/com/practice/Developer.java
@@ -1,5 +1,20 @@
-package com.practice;
+ package com.practice;
 
 public class Developer {
 
+    String name = "Parasuram";
+    int id = 101;
+    String role = "Java Developer";
+
+    public void display() {
+        System.out.println("Developer ID : " + id);
+        System.out.println("Developer Name : " + name);
+        System.out.println("Role : " + role);
+    }
+
+    public static void main(String[] args) {
+
+        Developer dev = new Developer();
+        dev.display();
+    }
 }
