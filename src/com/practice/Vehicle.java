2456a3058af4e9668f2ca1847e00bcf5fe21c196
parasuram423 <parasurams423@gmail.com>
2026-07-01 11:26:19 +0530

    Enhance Vehicle class with display method
    Added brand, model, and year fields to Vehicle class and implemented display method.

--- src/com/practice/Vehicle.java
+++ src/com/practice/Vehicle.java
@@ -1,10 +1,21 @@
-package com.practice;
+ package com.practice;
 
 public class Vehicle {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    String brand = "Honda";
+    String model = "Activa";
+    int year = 2024;
 
-	}
+    public void display() {
+        System.out.println("Brand : " + brand);
+        System.out.println("Model : " + model);
+        System.out.println("Year  : " + year);
+    }
 
+    public static void main(String[] args) {
+
+        Vehicle v = new Vehicle();
+        v.display();
+    }
+}
 }
