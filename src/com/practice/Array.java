71faf3bfaf9163a25ef58c569dcca9d054c0adbd
parasuram423 <parasurams423@gmail.com>
2026-07-01 11:24:23 +0530

    Add main method to Array class for printing elements

--- src/com/practice/Array.java
+++ src/com/practice/Array.java
@@ -1,5 +1,15 @@
-package com.practice;
+ package com.practice;
 
 public class Array {
 
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
